4274904a21e469d25ee4b6e5673f1fec78350167
Davi Silva <davisilvaphoto@gmail.com>
2024-12-30 14:54:54 -0300

    add controller and route logic

--- node-first-steps/src/index.js
+++ node-first-steps/src/index.js
@@ -1,13 +1,16 @@
 const http = require('http')
-const users = require('./mocks/users')
+const routes = require('./routes')
 
 const server = http.createServer((request, response) => {
-  if (request.url === '/users' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(users))
+  const route = routes.find(
+    (route) => route.endpoint === request.url && route.method === request.method
+  )
+
+  if (route) {
+    route.handler(request, response)
   } else {
-    response.writeHead(404, 'text/html')
-    response.end(`Canon ${request.method} ${request.url}`)
+    response.writeHead(404, { 'Content-Type': 'text/html' })
+    response.end(`Cannot ${request.method} ${request.url}`)
   }
 })
 
