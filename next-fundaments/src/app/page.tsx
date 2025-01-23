ad77341dadbe1ac1f5462f4bc8da8bf6a3d487b5
Davi Silva <davisilvaphoto@gmail.com>
2025-01-23 09:23:12 -0300

    client and server components

--- next-fundaments/src/app/page.tsx
+++ next-fundaments/src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 export default function Home() {
+  const { userAgent } = window?.navigator
+
   return (
     <div className="flex h-screen justify-center items-center">
       <h1>JStack - Next Fundaments</h1>
+      {JSON.stringify(userAgent)}
     </div>
   )
 }
